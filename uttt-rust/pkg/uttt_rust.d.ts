06e23e218a625d907eca0e3f8fdfebc60001e73b
TianLangHin <tianlanghin@gmail.com>
2024-12-11 14:49:04 +1100

    Updated default .gitignore inside pkg folder for WASM

---
+++ uttt-rust/pkg/uttt_rust.d.ts
@@ -0,0 +1,4 @@
+/* tslint:disable */
+/* eslint-disable */
+export function go(depth: string, board: string, side: boolean): (string)[];
+export function serialise_board(board_value: string): string;
